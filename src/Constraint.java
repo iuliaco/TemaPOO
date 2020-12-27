12a9385c9a39571b8fc28a5f5a700e9826e33c4f
iuliaco <iuliaco2001@gmail.com>
2020-12-27 18:47:32 +0200

    Terminat clasa Constraint, adaugat proprietati clasa Job

---
+++ src/Constraint.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+
+public class Constraint {
+    private LocalDate minDate, maxDate;
+    private int minExperience, maxExperience;
+    private Double minGPA, maxGPA;
+
+    public Constraint(LocalDate minDate, LocalDate maxDate) {
+        this.minDate = minDate;
+        this.maxDate = maxDate;
+    }
+
+    public Constraint(int minExperience, int maxExperience) {
+        this.minExperience = minExperience;
+        this.maxExperience = maxExperience;
+    }
+
+    public Constraint(Double minGPA, Double maxGPA) {
+        this.minGPA = minGPA;
+        this.maxGPA = maxGPA;
+    }
+
+    public boolean check(LocalDate date) {
+        int minYear = minDate.getYear();
+        int maxYear = maxDate.getYear();
+        int year = date.getYear();
+        if(minYear <= year) {
+            return maxYear >= year;
+        }
+        return false;
+    }
+    public boolean check(int experience) {
+        if(minExperience <= experience) {
+            return maxExperience >= experience;
+        }
+        return false;
+    }
+
+    public boolean check(Double GPA) {
+        if(minGPA <= GPA) {
+            return maxGPA >= GPA;
+        }
+        return false;
+    }
+}
--- src/Job.java
+++ src/Job.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+
 public class Job {
+    private String job, company;
+    private Boolean isOpen;
+    private Constraint graduationYear, yearsOfExperience, meanGPA;
+    private ArrayList<User> candidates;
+    private int numberOfPlaces;
+    private double salary;
 }
