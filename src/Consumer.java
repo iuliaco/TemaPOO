babd1d1fbf0865f0fad0b83b13ce61d0ae9c6a06
iuliaco <iuliaco2001@gmail.com>
2021-01-02 14:48:52 +0200

    Facut clasa Consumer, fara graf

--- src/Consumer.java
+++ src/Consumer.java
@@ -3,22 +3,27 @@ import java.util.ArrayList;
 public abstract class Consumer {
     Resume resume;
     ArrayList<Consumer> acquaintances;
+
     class Resume {
         private Information info;
         private SortedArrayList<Education> educations;
         private SortedArrayList<Experience> experiences;
+
         // educatie si experienta profi sortate
         public Resume() {
             educations = new SortedArrayList<Education>();
             experiences = new SortedArrayList<Experience>();
         }
+
         public Resume(Information info) {
             this();
             this.info = info;
         }
+
         public void addEducation(Education education) {
             this.educations.add(education);
         }
+
         public void addExperience(Experience experience) {
             this.experiences.add(experience);
         }
@@ -44,25 +49,43 @@ public abstract class Consumer {
     public void add(Education education) {
         resume.addEducation(education);
     }
+
     public void add(Experience experience) {
         resume.addExperience(experience);
     }
+
     public void add(Consumer consumer) {
         acquaintances.add(consumer);
     }
+
     public int getDegreeInFriendship(Consumer consumer) {
         return 1;
     }
+
     public void remove(Consumer consumer) {
         acquaintances.remove(consumer);
     }
+
     public Integer getGraduationYear() {
-        return 1;
-    }
-    public Double meanGPA() {
-        return 1.0;
+        for (Education education : resume.getEducations()) {
+            if (education.getEducationLevel().equals("licenta")) {
+                if (education.getEndDate() == null)
+                    return null;
+                else
+                    return education.getEndDate().getYear();
+            }
+        }
+        return null;
     }
 
+    public Double meanGPA() {
+        for (Education education : resume.getEducations()) {
+            if (education.getEducationLevel().equals("licenta")) {
+               return education.getMeanGPA();
+            }
+        }
+        return null;
+    }
 
 
 }
