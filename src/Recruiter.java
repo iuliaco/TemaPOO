d0afec7fe91f9d910d0f2bee220f894a439b8a5d
iuliaco <iuliaco2001@gmail.com>
2021-01-02 16:44:43 +0200

    Terminat clasa Recruiter

--- src/Recruiter.java
+++ src/Recruiter.java
@@ -1,10 +1,15 @@
 public class Recruiter extends Employee{
-    private int rating;
+    private double rating;
     public Recruiter(Resume resume) {
         super(resume);
         rating = 5;
     }
-    public int evaluate(Job job, User user) {
-
+    public double evaluate(Job job, User user) {
+        double score = rating * user.getTotalScore();
+        Request<Job, Consumer> request = new Request<>(job, user, this, score);
+        String company = this.getCompany();
+        Application application = Application.getInstance();
+        application.getCompany(company).getManager().addRequest(request);
+        return score;
     }
 }
